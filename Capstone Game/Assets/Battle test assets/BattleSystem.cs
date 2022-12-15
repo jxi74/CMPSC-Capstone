1ef9e108694167fc8cf13da2dce3eec6eb51ef6f
ghl5043 <82041046+ghl5043@users.noreply.github.com>
2022-12-15 05:59:37 -0500

    Attack button, dmg, basic effects
    attack button working
    health bar updates on taking damage
    
    kaboom.

--- Capstone Game/Assets/Battle test assets/BattleSystem.cs
+++ Capstone Game/Assets/Battle test assets/BattleSystem.cs
@@ -13,13 +13,15 @@ public class BattleSystem : MonoBehaviour
 	unit_data playerUnit;
 	unit_data enemyUnit;
 
+	
     // Start is called before the first frame update
     void Start()
     {
-        state = BattleState.start;
+        //state = BattleState.start;
 		setup();
     }
 
+	
     // Update is called once per frame
     void setup()
     {	
@@ -30,4 +32,20 @@ public class BattleSystem : MonoBehaviour
 		GameObject enemystuff = Instantiate(enemy);
 		enemyUnit = enemystuff.GetComponent<unit_data>();
     }
+
+	public void attack_button() {
+		
+		//get stats to calculate dmg
+		int atk = playerUnit.atk;
+		int def = enemyUnit.def;
+		int hp = enemyUnit.current_hp;
+		
+		int val = hp - (atk - def);
+
+		//update enemy hp ui
+		enemyUnit.update_health(val);
+		//if enemy less than 0 end battle
+
+		//explode enemy
+	}
 }
--- Capstone Game/Assets/Battle test assets/Healthbar.cs
+++ Capstone Game/Assets/Battle test assets/Healthbar.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Healthbar : MonoBehaviour
 {
     private Camera cam;
+    public GameObject particles;
     
+    public TextMeshProUGUI entity_name;
     public Slider ui_max_hp;
     public Slider ui_current_hp;
 
     void Start()
     {
+        
         cam = Camera.main;
+        particles.SetActive(false);
+    }
+
+    public void setName(string name)
+    {
+        entity_name.text = name;
     }
     public void updatemax(int hp)
     {
@@ -24,8 +34,13 @@ public class Healthbar : MonoBehaviour
 
     public void updatehp(int hp)
     {
+        if (hp <= 0)
+        {
+            particles.SetActive(true);
+        }
         ui_current_hp.value = (float)hp;
         
+
     }
 
     void Update()
