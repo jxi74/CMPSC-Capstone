7c5824c1a308e95719a4fecd9e4bffc681818c2b
ghl5043 <82041046+ghl5043@users.noreply.github.com>
2022-12-15 06:07:15 -0500

    Revert "Attack button, dmg, basic effects"
    This reverts commit 1ef9e108694167fc8cf13da2dce3eec6eb51ef6f.

--- Capstone Game/Assets/Battle test assets/BattleSystem.cs
+++ Capstone Game/Assets/Battle test assets/BattleSystem.cs
@@ -13,15 +13,13 @@ public class BattleSystem : MonoBehaviour
 	unit_data playerUnit;
 	unit_data enemyUnit;
 
-	
     // Start is called before the first frame update
     void Start()
     {
-        //state = BattleState.start;
+        state = BattleState.start;
 		setup();
     }
 
-	
     // Update is called once per frame
     void setup()
     {	
@@ -32,20 +30,4 @@ public class BattleSystem : MonoBehaviour
 		GameObject enemystuff = Instantiate(enemy);
 		enemyUnit = enemystuff.GetComponent<unit_data>();
     }
-
-	public void attack_button() {
-		
-		//get stats to calculate dmg
-		int atk = playerUnit.atk;
-		int def = enemyUnit.def;
-		int hp = enemyUnit.current_hp;
-		
-		int val = hp - (atk - def);
-
-		//update enemy hp ui
-		enemyUnit.update_health(val);
-		//if enemy less than 0 end battle
-
-		//explode enemy
-	}
 }
