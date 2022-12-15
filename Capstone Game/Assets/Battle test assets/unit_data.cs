4bb7406732cdd3f078e79c608127010377b83929
ghl5043 <82041046+ghl5043@users.noreply.github.com>
2022-12-14 22:47:51 -0500

    Battle test scene
    added stuff

---
+++ Capstone Game/Assets/Battle test assets/unit_data.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unit_data : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string unit_name;
+
+    public int max_hp;
+    public int current_hp;
+    public int atk;
+    public int def;
+}
