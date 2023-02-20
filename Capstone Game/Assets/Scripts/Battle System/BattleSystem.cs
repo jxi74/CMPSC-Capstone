54d3d8eaba40253518415efc8d4b0b3f32a39fa3
jxi74 <112279011+jxi74@users.noreply.github.com>
2023-02-20 02:15:59 -0500

    undo

--- Capstone Game/Assets/Scripts/Battle System/BattleSystem.cs
+++ Capstone Game/Assets/Scripts/Battle System/BattleSystem.cs
@@ -16,7 +16,6 @@ public class BattleSystem : MonoBehaviour
     [SerializeField] public List<int> targetlist;
 
     //UI
-    [SerializeField] private Canvas UI;
     [SerializeField] private BattleUI battleUI;
     [SerializeField] MessageBox box;
     [SerializeField] public SkillsSelect skillsSelect;
@@ -43,7 +42,6 @@ public class BattleSystem : MonoBehaviour
     {
         unithuds = new List<UnitUI> {unit1hud, unit2hud, unit3hud, unit4hud};
         StartCoroutine(BattleSetup());
-        UI.enabled = true;
         battleUI.SetupBattle();
     }
 
--- Capstone Game/Assets/Scripts/Overworld/Enemy.cs
+++ Capstone Game/Assets/Scripts/Overworld/Enemy.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class Enemy : MonoBehaviour, IEnemyInteractable
+public class Enemy : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _prompt;
     public string InteractionPrompt => _prompt;
     //[SerializeField] public ThirdPersonMovement playerMovement; // reference to ThirdPersonMovement script
     [SerializeField] public int distance;
 
-    public bool Interact(EnemyInteractor interactor)
+    public bool Interact(Interactor interactor)
     {
         Debug.Log("Interacting with enemy");
         ThirdPersonMovement playerMovement = interactor.GetComponent<ThirdPersonMovement>();
