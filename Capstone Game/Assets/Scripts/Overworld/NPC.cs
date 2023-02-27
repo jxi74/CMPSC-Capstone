6aa4fa01ea0acbdccbd7f2032161ce5e8793df8c
jxi74 <112279011+jxi74@users.noreply.github.com>
2023-02-26 22:33:11 -0500

    NPC Interaction

--- Capstone Game/Assets/Scripts/Overworld/NPC.cs
+++ Capstone Game/Assets/Scripts/Overworld/NPC.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,22 @@ public class NPC : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _prompt;
 
+    [SerializeField] OverWorldBox text;
+    [SerializeField] List<String> dialogue;
+
     public string InteractionPrompt => _prompt;
     
     public bool Interact(Interactor interactor) //Could have a check for the player's inventory to see if player has a key to open
     {
         Debug.Log("NPC Interaction"); // Logs message once you press "e" to open chest
+
+        foreach (String words in dialogue)
+        {
+            text.EnqueueSentence(words);
+            //yield return new WaitForSeconds(2);
+        }
+        text.DisplayNextSentences();
+
         return true;
     }
 }
---
+++ Capstone Game/Assets/Scripts/Overworld/OverWorldBox.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class OverWorldBox : MonoBehaviour
+{
+    [SerializeField] private Canvas overworldbox;
+    [SerializeField] private int lettersPerSec;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private GameObject contButton;
+    
+    public Queue<String> Sentences = new Queue<string>();
+    
+    public void SetText(String value)
+    {
+       text.text = value;
+    }
+    
+    public void EnqueueSentence(String value)
+    {
+       Sentences.Enqueue(value);
+       //Debug.Log("Sentence Enqueued!");
+    }
+
+    public void DisplayNextSentences()
+    {
+      
+       if (Sentences.Count == 0)
+       {
+          //Debug.Log("No more");
+          overworldbox.enabled = false;
+          return;
+       }
+       
+       StartCoroutine(DisplayText(Sentences.Dequeue()));
+      
+    }
+    
+    public IEnumerator DisplayText(String value)
+    {
+       overworldbox.enabled = true;
+       contButton.SetActive(false);
+       text.text = "";
+       foreach (var letter in value.ToCharArray())
+       {
+          text.text += letter;
+          yield return new WaitForSeconds(1f / lettersPerSec);
+       }
+ 
+       yield return new WaitForSeconds(1f);
+       contButton.SetActive(true);
+       //Delay to make text readable
+       //Use this code to enable elements
+       //overworldbox.enabled = false;
+    }
+}
